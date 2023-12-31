df4a7fc4c8bbbc6db8c2d05d954916a95619a4c5
Konrad Kubczyk <konrad@kubczyk.com>
2023-12-31 16:52:22 +0100

    feat: histories mockups

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -2,13 +2,40 @@
 const numberOfFields = 9;
 
 function fieldClicked(fieldNumber: number) {
-  console.log(`[DEBUG] Field ${fieldNumber} clicked.`);
+  console.log(`[DEBUG] Field ${fieldNumber} clicked (col: ${(fieldNumber - 1) % 3 + 1}, row: ${Math.floor((fieldNumber - 1) / 3) + 1})`);
 }
 </script>
 
 <template>
-  <section class="container w-1/3 mx-auto">
-    <div class="grid grid-cols-3 border rounded-xl text-center gap-4 p-4">
+  <section class="container mx-auto flex gap-4 w-2/3 text-gray-700">
+    <div class="border rounded-xl p-4 w-1/5">
+      <h2 class="uppercase tracking-wider font-semibold text-center">
+        History
+      </h2>
+      <table class="w-full">
+        <thead class="text-left">
+        <tr>
+          <th>#</th>
+          <th>Winner</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>1</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>O</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>X</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="flex-1 grid grid-cols-3 border rounded-xl text-center gap-4 p-4">
       <div
           v-for="fieldNumber in numberOfFields"
           :key="fieldNumber"
@@ -17,6 +44,41 @@ function fieldClicked(fieldNumber: number) {
       >
         {{ fieldNumber }}
       </div>
+    </div>
+    <div class="border rounded-xl p-4 w-1/5">
+      <h2 class="uppercase tracking-wider font-semibold text-center">
+        Moves
+      </h2>
+      <table class="w-full">
+        <thead class="text-left">
+        <tr>
+          <th>#</th>
+          <th>Player</th>
+          <th>Col</th>
+          <th>Row</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>1</td>
+          <td>X</td>
+          <td>3</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>O</td>
+          <td>2</td>
+          <td>1</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>X</td>
+          <td>1</td>
+          <td>3</td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   </section>
 </template>
