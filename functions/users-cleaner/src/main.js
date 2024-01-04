d1bd6bb845f904c4647b7c35276b74a898bcacc3
Konrad Kubczyk <konrad@kubczyk.com>
2024-01-04 20:35:21 +0100

    feat: implement old user accounts cleaner

--- functions/users-cleaner/src/main.js
+++ functions/users-cleaner/src/main.js
@@ -1,33 +1,32 @@
-import { Client } from 'node-appwrite';
+const sdk = require('node-appwrite');
 
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+const client = new sdk.Client();
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+const users = new sdk.Users(client);
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+const currentTimeStamp = Math.floor(Date.now() / 1000); // Get current timestamp in seconds
+const timeLimitSeconds = process.env.TIME_LIMIT_SECONDS; // Read time limit from env var, default to 10 minutes
+const thresholdTimestamp = currentTimeStamp - timeLimitSeconds;
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
+async function removeOldUsers() {
+    try {
+        const userList = await users.list(); // Get a list of all users
 
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
-};
+        for (const user of userList.users) {
+            const lastUpdate = user['lastUpdate'];
+            if (lastUpdate < thresholdTimestamp) {
+                console.log(`Deleting user: ${user.name} - ID: ${user.$id}`);
+                await users.delete(user.$id); // Delete the user
+            }
+        }
+        console.log('Old users removed successfully.');
+    } catch (error) {
+        console.error('Error removing old users:', error);
+    }
+}
+
+removeOldUsers();
