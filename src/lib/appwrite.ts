e5c2420744e49754ee0daf931770f219194b795e
Konrad Kubczyk <konrad@kubczyk.com>
2024-01-05 23:30:30 +0100

    fix: filetype

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,12 @@
+import { Client, Account, Databases, Functions } from 'appwrite';
+
+const client = new Client();
+
+client
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('6595d518075324972bbb');
+
+export const databases = new Databases(client);
+export const functions = new Functions(client);
+export const account = new Account(client);
+export { ID } from 'appwrite';
