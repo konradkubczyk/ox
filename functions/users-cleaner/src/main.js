6c8c8412a29bcb590376c40371f260ca286d3750
Konrad Kubczyk <konrad@kubczyk.com>
2024-01-06 20:22:18 +0100

    fix: remove inaccurate comments

--- functions/users-cleaner/src/main.js
+++ functions/users-cleaner/src/main.js
@@ -8,8 +8,8 @@ client
 
 const users = new sdk.Users(client);
 
-const currentTimeStamp = Date.now(); // Get current timestamp in seconds
-const timeLimitSeconds = process.env.TIME_LIMIT_SECONDS * 1000; // Read time limit from env var, default to 10 minutes
+const currentTimeStamp = Date.now();
+const timeLimitSeconds = process.env.TIME_LIMIT_SECONDS * 1000;
 const thresholdTimestamp = currentTimeStamp - timeLimitSeconds;
 
 export default async ({ req, res, log, error }) => {
@@ -19,7 +19,7 @@ export default async ({ req, res, log, error }) => {
     log(`- threshold timestamp: ${thresholdTimestamp}`);
     let deletedUsersCount = 0;
     try {
-        const userList = await users.list(); // Get a list of all users
+        const userList = await users.list();
         log('User list fetched successfully')
         for (const user of userList.users) {
             log(`Checking user ${JSON.stringify(user)}`);
@@ -27,7 +27,7 @@ export default async ({ req, res, log, error }) => {
             log(`- last access: ${lastAccess}`);
             log(`- time passed: ${currentTimeStamp - lastAccess}`);
             if (lastAccess < thresholdTimestamp) {
-                await users.delete(user.$id); // Delete the user
+                await users.delete(user.$id);
                 deletedUsersCount++;
             }
         }
