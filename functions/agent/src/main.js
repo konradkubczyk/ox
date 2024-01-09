ba4d50f0cfd58ac2251b42ba9af925e10b3f1b17
Konrad Kubczyk <konrad@kubczyk.com>
2024-01-09 20:21:09 +0100

    fix: suppress warnings

--- functions/agent/src/main.js
+++ functions/agent/src/main.js
@@ -4,7 +4,9 @@ import { createSession, joinSession, makeMove } from './session.js'
 export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
+    // eslint-disable-next-line no-undef
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    // eslint-disable-next-line no-undef
     .setKey(process.env.APPWRITE_API_KEY)
 
   const databases = new Databases(client)
